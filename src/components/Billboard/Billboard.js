4acb61237a135d4085946ef72a550ba261816cc2
broof716 <broof716@gmail.com>
2021-07-20 15:04:19 -0700

    Setting up Billboard component on front page

--- src/components/Billboard/Billboard.js
+++ src/components/Billboard/Billboard.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import billboardHeroImg from './assets/billboard.webp';
+import billboardHeroTitle from './assets/billboard-title.webp';
+
 import styles from './Billboard.module.css';
 function Billboard() {
-    return <div className={styles.billboard}>Billboard</div>;
+    return (
+        <div className={styles.billboard}>
+            <div className={styles.innerBillboard}>
+                <img src={billboardHeroImg} alt="" />
+                <div className={styles.info}>
+                    <img src={billboardHeroTitle} alt="Abstract: The Art of Design" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Billboard;
